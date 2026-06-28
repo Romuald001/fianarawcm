96596980126f7cc2fc8c7123c9ee0593c34ee78f
Romuald001 <romualdandronirina08@gmail.com>
2026-06-28 19:30:20 +0300

    fixed config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,8 @@
 const {Sequelize} = require ('sequelize');
 require('dotenv').config();
 
+const useSSL = process.env.DB_SSL === 'true';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -9,7 +11,14 @@ const sequelize = new Sequelize(
         host: process.env.DB_HOST,
         port: process.env.DB_PORT, 
         dialect: 'mysql', 
-        logging: false
+        logging: false,
+        dialectOptions: useSSL
+        ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        } : {},
     }
 );
 
